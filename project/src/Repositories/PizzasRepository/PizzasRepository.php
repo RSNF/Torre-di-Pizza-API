8f6ac44df31dca74435022f9ff52ba4bfb9d9cdb
RSNF <ray1001novaes@gmail.com>
2023-10-21 02:58:56 -0300

    refactor: PizzasRepository

---
+++ project/src/Repositories/PizzasRepository/PizzasRepository.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Repositories;
+
+use PizzasInterface;
+use RepoQueries;
+
+class PizzasRepository extends RepoQueries implements PizzasInterface
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    function create(array $data)
+    {
+        $sql = "INSERT INTO pizzaria.pizzas
+        (nome, tamanho_id, descricao)
+        VALUES ($1, $2, $3);";
+
+        return $this->doQueryParams($sql, $data);
+    }
+
+    function update(array $data)
+    {
+        $sql = "UPDATE pizzaria.pizzas SET 
+        nome = COALESCE($2, nome), 
+        tamanho_id = COALESCE($3, tamanho_id), 
+        descricao = $4
+        WHERE id = $1;";
+
+        return $this->doQueryParams($sql, $data);
+    }
+
+    function delete(string $id)
+    {
+        $sql = "DELETE FROM pizzaria.pizzas WHERE id = $1;";
+
+        return $this->doQueryParams($sql, array($id));
+    }
+
+    function find(string $id)
+    {
+        $sql = "SELECT * FROM pizzaria.pizzas WHERE id = $1";
+
+        return $this->getRowParams($sql, array($id));
+    }
+
+    function all()
+    {
+        $sql = "SELECT * FROM pizzaria.pizzas";
+
+        return $this->getRows($sql);
+    }
+}
+
+?>
