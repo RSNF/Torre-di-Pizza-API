b80cf5487998356e0a7765122f30a1dda6cc78f3
RSNF <ray1001novaes@gmail.com>
2023-10-22 02:32:45 -0300

    feat: RepoQueries class

---
+++ project/src/Repositories/RepoQueries.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Repository Queries
+ * @abstract
+ */
+abstract class RepoQueries
+{
+    /**
+     * @var PgSql\Connection
+     */
+    private $dbconn;
+
+    /**
+     * RepoQueries Constructor
+     */
+    public function __construct()
+    {
+        global $dbconn;
+        $this->$dbconn = $dbconn;
+    }
+
+    /**
+     * @param string $sql
+     * @param array $params
+     * @return array|bool
+     */
+    protected function getRowParams(string $sql, array $params)
+    {
+        $result = pg_query_params($this->dbconn, $sql, $params);
+        $row = pg_fetch_assoc($result);
+
+        return $row;
+    }
+
+    /**
+     * @param string $sql
+     * @return array
+     */
+    protected function getRows(string $sql)
+    {
+        $result = pg_query($this->dbconn, $sql);
+        $rows = pg_fetch_all($result);
+
+        return $rows;
+    }
+
+    /**
+     * @param string $sql
+     * @param array $params
+     * @return bool
+     */
+    protected function doQueryParams(string $sql, array $params)
+    {
+        $result = pg_query_params($this->dbconn, $sql, $params);
+
+        return ($result ? true : false);
+    }
+}
+
+?>
