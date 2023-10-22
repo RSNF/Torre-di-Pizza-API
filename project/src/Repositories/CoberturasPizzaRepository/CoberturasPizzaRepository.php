67d1d9ac3929ea891465b02c01b4fb11e2593429
RSNF <ray1001novaes@gmail.com>
2023-10-22 01:23:35 -0300

    refactor: CoberturasPizzaRepository

---
+++ project/src/Repositories/CoberturasPizzaRepository/CoberturasPizzaRepository.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Repositories;
+
+use CoberturasPizzaInterface;
+use RepoQueries;
+
+class CoberturasPizzaRepository extends RepoQueries implements CoberturasPizzaInterface
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    function create(array $data)
+    {
+        $sql = "INSERT INTO pizzaria.coberturas_pizza
+        (pizza_id, cobertura_id)
+        VALUES ($1, $2);";
+
+        return $this->doQueryParams($sql, $data);
+    }
+
+    function update(array $data)
+    {
+        $sql = "UPDATE pizzaria.coberturas_pizza SET 
+        pizza_id = COALESCE($2, pizza_id), 
+        cobertura_id = COALESCE($3, cobertura_id)
+        WHERE pizza_id = $1;";
+
+        return $this->doQueryParams($sql, $data);
+    }
+
+    function delete(string $id)
+    {
+        $sql = "DELETE FROM pizzaria.coberturas_pizza WHERE pizza_id = $1;";
+
+        return $this->doQueryParams($sql, array($id));
+    }
+
+    function find(string $id)
+    {
+        $sql = "SELECT * FROM pizzaria.coberturas_pizza WHERE pizza_id = $1";
+
+        return $this->getRowParams($sql, array($id));
+    }
+
+    function all()
+    {
+        $sql = "SELECT * FROM pizzaria.coberturas_pizza";
+
+        return $this->getRows($sql);
+    }
+}
+
+?>
