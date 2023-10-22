d0de136bc90c8925493b2230fe54c4417054ef21
RSNF <ray1001novaes@gmail.com>
2023-10-22 02:15:31 -0300

    refactor: PedidoPizzasRepository

---
+++ project/src/Repositories/PedidoPizzasRepository/PedidoPizzasInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+interface PedidoPizzasInterface
+{
+    /**
+     * @param array $data
+     * @return bool
+     */
+    public function create(array $data);
+
+    /**
+     * @param array $data
+     * @return bool
+     */
+    public function update(array $data);
+
+    /**
+     * @param string $id
+     * @return bool
+     */
+    public function delete(string $id);
+
+    /**
+     * @param string $id
+     * @return array
+     */
+    public function find(string $id);
+
+    /**
+     * @return array
+     */
+    public function all();
+}
+
+?>
---
+++ project/src/Repositories/PedidoPizzasRepository/PedidoPizzasRepository.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Repositories;
+
+use PedidoPizzasInterface;
+use RepoQueries;
+
+class PedidoPizzasRepository extends RepoQueries implements PedidoPizzasInterface
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    function create(array $data)
+    {
+        $sql = "INSERT INTO pizzaria.pedido_tem_pizzas
+        (pedido_id, pizza_id)
+        VALUES ($1, $2);";
+
+        return $this->doQueryParams($sql, $data);
+    }
+
+    function update(array $data)
+    {
+        $sql = "UPDATE pizzaria.pedido_tem_pizzas SET 
+        pedido_id = COALESCE($2, pedido_id), 
+        pizza_id = COALESCE($3, pizza_id)
+        WHERE pedido_id = $1;";
+
+        return $this->doQueryParams($sql, $data);
+    }
+
+    function delete(string $id)
+    {
+        $sql = "DELETE FROM pizzaria.pedido_tem_pizzas WHERE pedido_id = $1;";
+
+        return $this->doQueryParams($sql, array($id));
+    }
+
+    function find(string $id)
+    {
+        $sql = "SELECT * FROM pizzaria.pedido_tem_pizzas WHERE pedido_id = $1";
+
+        return $this->getRowParams($sql, array($id));
+    }
+
+    function all()
+    {
+        $sql = "SELECT * FROM pizzaria.pedido_tem_pizzas";
+
+        return $this->getRows($sql);
+    }
+}
+
+?>
